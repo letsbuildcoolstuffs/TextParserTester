6c77698fbb6b3ffe5093bbe77e2feacfe9cbacca
Chris Gamer <Chris Gamer@GAMINGTOSPACE>
2024-01-07 05:24:09 -0500

    Add project files.

---
+++ TextParserTester/Program.cs
@@ -0,0 +1,32 @@
+﻿var folder = @"C:\Image Reader\Main";
+
+//Filename
+var fileName = "KeyWords.txt";
+var fullPath = folder + fileName;
+string? readText = null;
+
+try
+{
+    if (File.Exists(fullPath))
+    {
+        //Open the file to read from.
+        readText = File.ReadAllText(fullPath);
+    }
+}
+catch (Exception)
+{
+    //File could not be opened.
+    readText = null;
+}
+
+
+if (string.IsNullOrEmpty(readText))
+{
+}
+
+
+/*
+Notes:
+Pseudo code logic:
+Idea will be to read in screenshots, parse, and prep a sales list to skip alt tab process.
+*/
